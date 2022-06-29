03086867b06ac63c5a69be2260084ac668d6f316
kimminju99 <mindori1019@gmail.com>
2022-06-29 17:04:31 +0900

    List UI 구현

--- app/src/main/java/com/kimminju99/recyclerview/MainActivity.kt
+++ app/src/main/java/com/kimminju99/recyclerview/MainActivity.kt
@@ -3,14 +3,19 @@ package com.kimminju99.recyclerview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kimminju99.recyclerview.ui.theme.RecyclerviewTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,6 +40,17 @@ fun MyApp() {
 
 @Composable
 fun RecyclerViewContent() {
+    val puppies = remember { DataProvider.puppyList }
+    LazyColumn(contentPadding = PaddingValues(16.dp, 8.dp)) {
+        items(
+            items = puppies,
+            itemContent = { PuppyListItem(it) }
+        )
+    }
+}
+
+@Composable
+fun PuppyListItem(puppy: Puppy) {
 
 }
 
