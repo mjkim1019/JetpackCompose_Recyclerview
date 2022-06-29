41794132db8bc79a5234a55b1e67183a731c00c0
kimminju99 <mindori1019@gmail.com>
2022-06-29 15:41:50 +0900

    Initial commit

---
+++ app/src/main/java/com/kimminju99/recyclerview/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.kimminju99.recyclerview
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kimminju99.recyclerview.ui.theme.RecyclerviewTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RecyclerviewTheme {
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    Scaffold(
+        backgroundColor = MaterialTheme.colors.background
+    ) {
+        RecyclerViewContent()
+    }
+}
+
+@Composable
+fun RecyclerViewContent() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RecyclerviewTheme {
+        MyApp()
+    }
+}
