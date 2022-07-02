a7fcc40460f0e8b33036853d7bb623f00e4878df
kimminju99 <mindori1019@gmail.com>
2022-07-03 00:36:42 +0900

    CardView 완성

--- app/src/main/java/com/kimminju99/recyclerview/MainActivity.kt
+++ app/src/main/java/com/kimminju99/recyclerview/MainActivity.kt
@@ -3,17 +3,20 @@ package com.kimminju99.recyclerview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -56,11 +59,22 @@ fun RecyclerViewContent() {
 
 @Composable
 fun PuppyListItem(puppy: Puppy) {
-    Row {
-        PuppyImage(puppy = puppy)
-        Column {
-            Text(text = puppy.name, style = Typography.h6)
-            Text(text = puppy.content, style = Typography.caption)
+    Card(
+        modifier = Modifier.fillMaxWidth().padding(0.dp, 12.dp),
+        elevation = 4.dp,
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        border = BorderStroke(1.dp, MaterialTheme.colors.primary)
+    ) {
+        Row {
+            PuppyImage(puppy = puppy)
+            Column(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(text = puppy.name, style = Typography.h6)
+                Text(text = puppy.content, style = Typography.caption, modifier = Modifier.padding(0.dp,2.dp))
+            }
         }
     }
 }
